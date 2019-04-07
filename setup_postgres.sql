521783e866228b6a1ae44e702c25d1fe4ebca0a0
Alexis Leclerc <alexis@alxs.fr>
2019-04-07 18:30:10 +0300

    [Travis] setup CI

--- setup_postgres.sql
+++ setup_postgres.sql
@@ -1,4 +1,7 @@
 CREATE ROLE shared_world WITH LOGIN CREATEDB PASSWORD 'password';
+CREATE DATABASE shared_world_production WITH OWNER shared_world;
+CREATE DATABASE shared_world_development WITH OWNER shared_world;
+CREATE DATABASE shared_world_test WITH OWNER shared_world;
 
 \c shared_world_development
 CREATE EXTENSION pgcrypto;
