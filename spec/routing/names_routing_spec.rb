f9eff3df84ce0e828c5e2635b72c84d1187dcb9d
Alexis Leclerc <alexis@alxs.fr>
2019-06-03 13:59:07 +0300

    Get /names/generate change default size to 4

--- spec/routing/names_routing_spec.rb
+++ spec/routing/names_routing_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe NamesController, type: :routing do
   describe 'routing' do
     it 'routes to #generate with default param' do
-      expect(get: '/names/generate').to route_to('names#generate', syllables_count: '3')
+      expect(get: '/names/generate').to route_to('names#generate', syllables_count: '4')
     end
 
     it 'routes to #generate with expected param' do
