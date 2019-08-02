f9490263aad909224eac6791e53a77d9b8f127d2
Alexis Leclerc <alexis.leclerc@data.gouv.fr>
2019-08-02 18:24:59 +0300

    Model: Assault::Antagonist

---
+++ app/engines/assault/antagonist.rb
@@ -0,0 +1,30 @@
+class Assault::Antagonist
+  include ActiveModel::Model
+  include Assault::Alive
+  include Assault::Taggable
+
+  attr_accessor :name, :damages, :damage_range
+
+  def initialize(damages:, **args)
+    @result = {}
+    @remaining_damages = damages
+    super
+  end
+
+  protected
+
+  def dispatch_damages_to(targets)
+    result = Assault::DamageDispatcher.call(
+      total_damages: @remaining_damages,
+      damage_range: @damage_range,
+      targets: targets)
+
+    @remaining_damages -= result.values.sum
+
+    merge_result_with result
+  end
+
+  def merge_result_with(intermediary_result)
+    @result.merge! intermediary_result
+  end
+end
---
+++ spec/engines/assault/antagonist_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe Assault::Antagonist do
+  subject do
+    described_class.new(
+      name: 'Name',
+      tags: [:something, :nothing],
+      damages: 5,
+      damage_range: 1..2,
+      health_points: 10,
+      max_health_points: 100
+    )
+  end
+
+  let(:object) { Object.new }
+
+  its(:name) { is_expected.to eq 'Name' }
+  its(:tags) { are_expected.to include :something, :nothing }
+  its(:damages) { are_expected.to eq 5 }
+  its(:damage_range) { is_expected.to eq 1..2 }
+  its(:health_points) { are_expected.to eq 10 }
+  its(:max_health_points) { are_expected.to eq 100 }
+end
