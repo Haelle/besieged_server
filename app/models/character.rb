07764c415d74aa1be5f2ab6aa197d7b0fb87fa52
Alexis Leclerc <alexis@alxs.fr>
2020-03-04 11:14:37 +0200

    Character has action_points
    default to 0 and maxed at 24
    with regeneration_rate of 6

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,15 +1,30 @@
 class Character < ApplicationRecord
   validates :pseudonym, presence: true
-  validate :only_one_character_per_camp
+  validate :only_one_character_per_camp,
+    :action_points_must_be_within_allowed_range
 
   belongs_to :account
   belongs_to :camp
 
+  def max_action_points
+    24
+  end
+
+  def action_point_regeneration_rate
+    6
+  end
+
   def exhausted?
-    false
+    action_points.zero?
   end
 
   private
+
+  def action_points_must_be_within_allowed_range
+    return if action_points.between?(0, max_action_points)
+
+    errors.add(:action_points, "action_points cannot be lower than 0 and greater thant #{max_action_points}")
+  end
 
   def only_one_character_per_camp
     return if camp.nil? || no_character_already_in_camp?
