894776b6d3bf0daac5ccdc8637d3de027a8a33d8
Alexis Leclerc <alexis@alxs.fr>
2020-03-04 11:18:05 +0200

    Update Spec config
    change display when running single file

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -65,6 +65,9 @@ RSpec.configure do |config|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
 
+  # use different formatter is running only one spec
+  config.default_formatter = config.files_to_run.one? ? 'doc' : 'progress'
+
   # rspec-mocks config goes here. You can use an alternate test double
   # library (such as bogus or mocha) by changing the `mock_with` option here.
   config.mock_with :rspec do |mocks|
